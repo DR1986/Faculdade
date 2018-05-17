a7b6c8160bb9a7fa881f31d04d883583bd464650
Ramon Cavalcante <ramoncavpires@gmail.com>
2018-05-17 18:05:00 -0300

    Add files via upload

---
+++ 1periodo/Linguagem de Programação I/16-05-2018/ac_listas_16-05.py
@@ -0,0 +1,27 @@
+'''
+======== Atividade Continua =========
+ Nome : Ramon C. Pires
+ R.A. : 1800260
+ S.I. - 1ºD noturno
+=====================================
+'''
+
+#Escreva um programa que peça para o usuário entrar com
+#um número n e utilize o Crivo de Eratóstenes
+#para encontrar todos os primos menores ou iguais a n.
+
+def criarLista(n):
+    lista=[]
+    for i in range(2, n+1):
+        lista.append(i)
+    return lista
+
+def primos(lista):
+    primos=[]
+    tot = 0
+    for i in lista:
+       if (i%2!=0 or i==2) and (i%3!=0 or i==3) and (i%5!=0 or i==5) and (i%7!=0 or i==7):
+           primos.append(i)    
+    return primos
+print(primos(criarLista(n=int(input("Digite um número: ")))))
+
